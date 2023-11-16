9d7c298880a215ce5601260adaf5793bad7293ca
Grigorii Bykov <g.m.bykov@tinkoff.ru>
2023-11-16 17:05:27 +0300

    Add team grid and update gradle

--- gradle/plugins/plugins-platform/build.gradle.kts
+++ gradle/plugins/plugins-platform/build.gradle.kts
@@ -6,8 +6,8 @@ plugins {
 dependencies.constraints {
     api("com.android.tools.build:gradle:8.0.2")
     api("com.autonomousapps:dependency-analysis-gradle-plugin:1.2.0")
-    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.10")
+    api("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.20")
     api("de.mannodermaus.gradle.plugins:android-junit5:1.8.2.0")
     api("com.klaxit.hiddensecrets:HiddenSecretsPlugin:0.2.1")
-    api("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.8.10-1.0.9")
+    api("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.8.20-1.0.10")
 }
